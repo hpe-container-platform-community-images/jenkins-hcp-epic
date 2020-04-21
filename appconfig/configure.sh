441d323a08f3eb70fddc2b6dbac9c3c92a900414
Chris Crawford <chriscrawford@Chriss-MacBook-Pro.local>
2020-04-21 09:31:36 -0500

    initial push

---
+++ appconfig/configure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SELF=${BASH_SOURCE[0]}
+export CONFIG_BASE_DIR=$(dirname ${SELF})
+
+source ${CONFIG_BASE_DIR}/logging.sh
+source ${CONFIG_BASE_DIR}/utils.sh
+source ${CONFIG_BASE_DIR}/macros.sh
+
+# App specific configuration here.
